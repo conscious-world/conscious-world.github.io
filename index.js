71a216837c02ba996813c8520c6957d41289ea39
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-04-04 10:00:55 -0700

    remove particles

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
+// particlesJS.load('particles-js', 'particles.json', function() {
+//   console.log('particles.js loaded - callback');
+// });
